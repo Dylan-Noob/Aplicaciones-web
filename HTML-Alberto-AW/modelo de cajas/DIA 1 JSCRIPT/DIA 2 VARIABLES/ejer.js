f4367702a49f03270a7ed5bf150c71f0ab5ee127
Dylan-Noob <drojaso@iesch.org>
2024-11-15 14:17:07 +0100

    15NOVIEMBRE

---
+++ HTML-Alberto-AW/modelo de cajas/DIA 1 JSCRIPT/DIA 2 VARIABLES/ejer.js
@@ -0,0 +1,26 @@
+//let numero = 5
+//let doble = (numero * 2)
+//let triple = (numero * 3)
+
+//console.log ("el doble de " + 5 + " es " + doble + " y el triple es " + triple)
+
+//let dias = 10
+
+//let nota1 = 2
+//let nota2 = 6
+//let nota3 = 8
+
+//let resultado = (nota1 > nota2) * "La nota1 es mayor que la edad2 " + ( nota2 > nota1) * "La nota2 es mayor que la nota2"
+//console.log
+
+let edad1 = 56;
+let edad2 = 96;
+
+if ( edad1 > edad2 ) {
+    console.log("edad1 es mayor que edad2. ");
+} else {
+
+   if (edad1 == edad2) {
+    console.log("edad1 es exactamente igual que edad1")
+   }
+}
---
+++ HTML-Alberto-AW/modelo de cajas/DIA 1 JSCRIPT/DIA 2 VARIABLES/nuevo.js
@@ -0,0 +1,5 @@
+let a = 25;
+let b = 10;
+let c = 5;
+
+console.log( a == b );
